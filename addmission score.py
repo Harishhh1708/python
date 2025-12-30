2c0471453da7fdeb36d40ea2e82b78140f33b2f6
Harishhh1708 <harishhh1708hk@gmail.com>
2025-12-30 20:53:28 +0530

    if else

---
+++ addmission score.py
@@ -0,0 +1,8 @@
+score=int(input("score:"))
+if(score>=70):
+    name=input("name:")
+    age=input("age:")
+    location=input("location:")
+    print("you are eligible")
+else:
+    print("not eligible")
